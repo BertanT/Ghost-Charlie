c6c45cd439293fa9aff42e875f779d17e523a5b8
Mehmet Bertan Tarakcioglu <83073748+BertanT@users.noreply.github.com>
2024-03-18 00:00:02 -0400

    Implement new footer

---
+++ assets/js/kitty.js
@@ -0,0 +1,14 @@
+// Created on March 16, 2024 by Mehmet Bertan Tarakcioglu
+
+// Little easter egg for the console because I think this ASCII cat is really cute
+// If you are concerned about performance, comment out the script tag in home.hbs
+
+function printKitty() {
+  console.log("%c\
+  /\\_/\\\n\
+=( ºwº )=\n\
+  )   (  //\n\
+ (＿ ＿)//\n\n\
+Good job! You found the kitty :)\n",
+              "font-family: monospace;")
+}
